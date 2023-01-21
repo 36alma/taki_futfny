1e635f09c18c0839f782bde002b83c634de83f86
36alma <85577434+36alma@users.noreply.github.com>
2023-01-21 18:26:47 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -5,22 +5,31 @@ input.onButtonPressed(Button.A, function () {
 })
 function RÉSZ () {
     for (let index = 0; index < 5; index++) {
+        a += 15
+        f = a
+        led.setBrightness(f)
         led.plot(X, Y)
         basic.pause(100)
         led.unplot(X, Y)
         X += 1
     }
     for (let index = 0; index < 5; index++) {
+        a += 15
+        f = a
+        led.setBrightness(f)
         led.plot(X, Y)
         basic.pause(100)
         led.unplot(X, Y)
         X += -1
     }
 }
+let a = 0
+let f = 0
 let X = 0
 let Y = 0
 Y = 2
 X = 0
+f = 0
 basic.forever(function () {
 	
 })
